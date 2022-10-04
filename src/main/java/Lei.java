dbd0bcfdbe7b75c5e3125e75674128a940dacd62
yonghuminglhj <youxianglhj.com>
2022-10-04 21:02:42 +0800

    最新

--- src/main/java/Lei.java
+++ src/main/java/Lei.java
@@ -2,7 +2,7 @@ public class Lei {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("xinfengzhi");
-        System.out.println("");
+        System.out.println("最新");
         System.out.println("");
         System.out.println("");
     }
